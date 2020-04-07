91ed3d5b209bf9db5ca39ba20c76ac334400138e
olivier chabot <chabot.oliv@gmail.com>
2020-04-07 16:37:47 +0200

    Updates

---
+++ precache-manifest.41f8dc4064fd344312301ccbc13aeb58.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "518aa84efa3aef9fe80b2f27ef08e51d",
+    "url": "/MetricsServer/index.html"
+  },
+  {
+    "revision": "f944456846381f6e22c5",
+    "url": "/MetricsServer/static/css/2.57aec81e.chunk.css"
+  },
+  {
+    "revision": "bb7ffc5d20c43bf99c69",
+    "url": "/MetricsServer/static/css/main.8214d0cf.chunk.css"
+  },
+  {
+    "revision": "f944456846381f6e22c5",
+    "url": "/MetricsServer/static/js/2.5d04cf83.chunk.js"
+  },
+  {
+    "revision": "8bb92c23487eb5c952e51992947cb95e",
+    "url": "/MetricsServer/static/js/2.5d04cf83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb7ffc5d20c43bf99c69",
+    "url": "/MetricsServer/static/js/main.45ddbadf.chunk.js"
+  },
+  {
+    "revision": "2b49be8eac3fb71ecc6e",
+    "url": "/MetricsServer/static/js/runtime-main.15ee5bca.js"
+  },
+  {
+    "revision": "def94e70c0c9ecf77e17fffcfa4858c1",
+    "url": "/MetricsServer/static/media/calendar.def94e70.svg"
+  }
+]);
